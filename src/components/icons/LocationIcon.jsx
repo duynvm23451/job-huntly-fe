fa2569754ad7fe5a4ad2da2a821a2c37f1e571a3
duy23451 <duynvm1711@gmail.com>
2024-08-05 12:21:58 +0900

    add show preview pdf

--- src/components/icons/LocationIcon.jsx
+++ src/components/icons/LocationIcon.jsx
@@ -2,7 +2,33 @@ import locationIcon from "../../assets/icons/location-icon.svg";
 
 const LocationIcon = ({ ...props }) => {
   return (
-    <img src={locationIcon} width={"30px"} alt="location-icon" {...props} />
+    <svg
+      {...props}
+      width="33"
+      height="32"
+      viewBox="0 0 33 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M19.7604 14.0005C19.7604 12.1588 18.2681 10.6665 16.4278 10.6665C14.5861 10.6665 13.0938 12.1588 13.0938 14.0005C13.0938 15.8409 14.5861 17.3332 16.4278 17.3332C18.2681 17.3332 19.7604 15.8409 19.7604 14.0005Z"
+        stroke="#25324B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16.429 28C14.8311 28 6.42969 21.1978 6.42969 14.0844C6.42969 8.51552 10.9058 4 16.429 4C21.9523 4 26.4297 8.51552 26.4297 14.0844C26.4297 21.1978 18.027 28 16.429 28Z"
+        stroke="#25324B"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
   );
 };
 
--- src/pages/employee/Profile.jsx
+++ src/pages/employee/Profile.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import coverImage from "@/assets/image/cover.jpg";
+import defaultImage from "@/assets/image/default-avatar.png";
+import MailIcon from "@/components/icons/MailIcon";
+import LocationIcon from "@/components/icons/LocationIcon";
+import RectangleButton from "@/components/shared/RectangleButton";
+import testPdf from "@/assets/pdf/testPdf.pdf";
+// Import the main component
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+
+// Import the styles
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import DateOfBirthIcon from "@/components/icons/DateOfBirthIcon";
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -18,8 +30,67 @@ const Profile = () => {
         </button>
       </div>
       <div className="px-6 pt-4 border-t-1 border-custom-neutral-2 grid grid-cols-12 space-x-6">
-        <div className="h-screen bg-red-400 col-span-8"></div>
-        <div className="h-screen bg-green-400 col-span-4"></div>
+        <div className="h-screen col-span-8">
+          <div className="w-full bg-white border-1 border-custom-neutral-2">
+            <img
+              src={coverImage}
+              alt="cover"
+              className="h-28 w-full object-cover object-center"
+            />
+            <div className="flex p-4">
+              <img
+                src={defaultImage}
+                alt="profile"
+                className="w-24 h-24 rounded-full -mt-16 ml-4 border-4 border-white"
+              />
+              <div className="ml-6">
+                <h1 className="text-xl font-bold whitespace-nowrap">
+                  Nguyen Van Manh Duy
+                </h1>
+                <p className="flex items-center mt-2">
+                  <MailIcon className="w-6 h-6 mr-2" /> duynvm1711@gmail.com
+                </p>
+                <p className="flex items-center mt-1 mb-4">
+                  <LocationIcon className="w-6 h-6 mr-2" />
+                  Manchester, UK
+                </p>
+              </div>
+              <div className="w-full text-end">
+                <RectangleButton type={"outline"}>
+                  Chỉnh sửa hồ sơ
+                </RectangleButton>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 bg-white border-1 border-custom-neutral-2 px-6 pt-4 pb-6">
+            <h1 className="text-xl font-semibold">Giới thiệu</h1>
+            <p className="mt-2">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora incidunt ut labore
+              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+              voluptas nulla pariatur?
+            </p>
+          </div>
+          <div className="mt-6 border-1 border-custom-neutral-2 h-full py-8 px-6">
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+              <Viewer fileUrl={testPdf} />
+            </Worker>
+          </div>
+          <div className="h-8"></div>
+        </div>
+        <div className="h-screen bg-green-400 col-span-4">
+          <div className="w-full bg-white border-1 border-custom-neutral-2 h-48">
+            <DateOfBirthIcon />
+          </div>
+        </div>
       </div>
     </div>
   );
